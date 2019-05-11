5d80b56996eabdb7dcbe43c9e9aea64cb4c60ef2
gchronos <adquasarium@gmail.com>
2019-05-11 17:47:38 +0300

    add on node to firestore

--- db-queue/index.js
+++ db-queue/index.js
@@ -5,19 +5,18 @@ const StorageCollectionKey = 'videoQueue';
 const DB = admin.firestore().collection(StorageKey);
 
 module.exports = {
-    addQueue: function(queueItem) {
+    addQueue: async function(queueItem) {
         const queueType = queueItem.queueType;
         const queueRef = DB.doc(queueType);
-        const increment = admin.firestore.FieldValue.increment(1);
 
         // push to collection new queueItem
         queueRef.collection(StorageCollectionKey).add(queueItem);
-        // create an empty document, or do nothing if it exists.
-        queueRef.set({}, { merge: true });
+
         // increment videoQueueLen
-        queueRef.update({
+        const increment = admin.firestore.FieldValue.increment(1);
+        queueRef.set({
             videoQueueLen: increment,
             queueType
-        });
+        }, { merge: true });
     }
 };
