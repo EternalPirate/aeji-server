f15f476186f4fe799a0cca68e5901c1328e2f47d
gchronos <adquasarium@gmail.com>
2019-05-12 10:04:46 +0300

    test

--- index.js
+++ index.js
@@ -58,9 +58,3 @@ socket.on('donation', (msg) => {
 
 
 
-require('dns').lookup(require('os').hostname(), function (err, ip) {
-    require('ping').sys.probe(ip, function(isAlive) {
-        const msg = isAlive ? 'host ' + ip + ' is alive' : 'host ' + ip + ' is dead';
-        console.log(msg);
-    });
-});
