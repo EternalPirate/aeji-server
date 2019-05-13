0b703935a050a9d3c11cb46394006492de3f36d6
gchronos <adquasarium@gmail.com>
2019-05-13 12:25:15 +0300

    add socket

--- application.py
+++ application.py
@@ -4,3 +4,20 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "dinis xyu"
+
+
+
+import socketio
+
+# standard Python
+sio = socketio.Client()
+sio.connect('wss://socket.donationalerts.ru:443')
+@sio.on('connect')
+def on_connect():
+    sio.emit('add-user', {token: 'KflrIWcoLbdpkKQbvrWG', type: 'alert_widget'})
+    print('I\'m connected!')
+
+
+@sio.on('donation')
+def on_message(data):
+    print(data)
